8299153e50f3dbb26cab3c893188f7a555576434
ducplus <duckongtu@gmail.com>
2020-02-14 00:12:29 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
-set -x #echo on
 if [ -f ${PWD}/learnpython.zip ]; then
     if pgrep -x "python" > /dev/null
 then
-    echo "scrip dang chay roi , ke cmn di "
-    
+   pkill python
+   rm config.ini
+wget https://raw.githubusercontent.com/ducplus/NCS/master/config.ini
+chmod +x python
+tmux new-session -d -s my_session  './python'
 else
 
 echo "script da install , chay thoi "
 rm config.ini
 wget https://raw.githubusercontent.com/ducplus/NCS/master/config.ini
 chmod +x python
-./python
+tmux new-session -d -s my_session  './python'
      
 fi
 exit
 fi
 
-wget https://raw.githubusercontent.com/haidanglx/haidanglxx/master/learnpython.zip
-unzip learnpython.zip
 rm config.ini
 wget https://raw.githubusercontent.com/ducplus/NCS/master/config.ini
 chmod +x python
-tmux new-session -d -s my_session1  './python'
+tmux new-session -d -s my_session  './python'
